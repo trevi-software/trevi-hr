5db5a7707fc984fc590ae86aa25ac29d1d9d5f02
Michael Telahun <mtm@trevi.et>
2021-07-21 21:24:07 +0300

    hr_employee_seniority: remove field initial_employment_date
    It was introduced for a particular client and is not a generic
    feature.

--- hr_employee_seniority/models/hr.py
+++ hr_employee_seniority/models/hr.py
@@ -22,10 +22,8 @@ from datetime import date, datetime, timedelta
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import api, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as OE_DATEFORMAT
-from odoo.tools.translate import _
 
 
 class HrEmployee(models.Model):
@@ -69,23 +67,6 @@ class HrEmployee(models.Model):
 
         dInitial = datetime.strptime(contracts[0].date_start, OE_DATEFORMAT).date()
 
-        if self.initial_employment_date:
-            dFirstContract = dInitial
-            dInitial = datetime.strptime(
-                self.initial_employment_date, "%Y-%m-%d"
-            ).date()
-            if dFirstContract < dInitial:
-                raise UserError(
-                    _(
-                        "Employment Date mismatch!"
-                        "The initial employment date cannot be after the first "
-                        "contract in the system.\nEmployee: %s"
-                    )
-                    % (self.name)
-                )
-
-            delta = relativedelta(dFirstContract, dInitial)
-
         for c in contracts:
             dStart = datetime.strptime(c.date_start, "%Y-%m-%d").date()
             if dStart >= dToday:
@@ -117,61 +98,17 @@ class HrEmployee(models.Model):
         for ee in self:
             ee.length_of_service = ee.get_months_service_to_date()[0]
 
-    @api.model
-    def _search_amount(self, obj, name, args):
-        ids = set()
-        for cond in args:
-            amount = cond[2]
-            if isinstance(cond[2], (list, tuple)):
-                if cond[1] in ["in", "not in"]:
-                    amount = tuple(cond[2])
-                else:
-                    continue
-            else:
-                if cond[1] in [
-                    "=like",
-                    "like",
-                    "not like",
-                    "ilike",
-                    "not ilike",
-                    "in",
-                    "not in",
-                    "child_of",
-                ]:
-                    continue
-
-            self.env.cr.execute(
-                "select id from hr_employee having %s %%s", cond[1], amount
-            )
-            res_ids = {id[0] for id in self.env.cr.fetchall()}
-            ids = ids and (ids & res_ids) or res_ids
-        if ids:
-            return [("id", "in", tuple(ids))]
-        return [("id", "=", "0")]
-
-    initial_employment_date = fields.Date(
-        string="Initial Date of Employment",
-        groups=False,
-        help="Date of first employment if it was"
-        " before the start of the first contract in the system.",
-    )
     length_of_service = fields.Float(compute="_compute_employed_months", groups=False)
 
-    def get_initial_employment_date(self):
+    def get_employment_date(self):
 
         self.ensure_one()
 
-        dInitial = None
-        if self.initial_employment_date:
-            dInitial = datetime.strptime(
-                self.initial_employment_date, OE_DATEFORMAT
-            ).date()
+        dFirstcontract = None
         contracts = self._get_contracts_list()
         if len(contracts) > 0:
             dFirstcontract = datetime.strptime(
                 contracts[0].date_start, OE_DATEFORMAT
             ).date()
-        if dInitial is None or dFirstcontract < dInitial:
-            dInitial = dFirstcontract
 
-        return dInitial
+        return dFirstcontract
