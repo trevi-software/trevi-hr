07d6331e0909e72ae65a3fc1755a20b936d22857
Michael Telahun <mtm@trevi.et>
2021-07-22 11:19:20 +0300

    hr_employee_seniority: mostly add tests
    Includes:
    o Fixes done because of tests
    o Shorten copyright
    o rename models/hr.py -> models/hr_employee.py

--- hr_employee_seniority/tests/__init__.py
+++ hr_employee_seniority/tests/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2021 Trevi Software (https://trevi.et)
-# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import test_hr_employee
