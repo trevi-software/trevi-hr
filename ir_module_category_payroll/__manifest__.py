27987a622181de478178175573889c0602a23981
Michael Telahun <mtm@trevi.et>
2021-09-18 11:53:57 +0300

    [ADD] ir_module_category_payroll: creates a new Payroll module category

---
+++ ir_module_category_payroll/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2021 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Payroll Module Category",
+    "summary": "Creates Payroll module category",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "author": "TREVI Software, Michael Telahun Makonnen",
+    "license": "AGPL-3",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/ir_module_category.xml",
+    ],
+    "installable": True,
+}
