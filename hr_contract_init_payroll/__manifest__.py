09b00d98a641e1445fc8adb1ab05f4e56f41ab91
Michael Telahun <mtm@trevi.et>
2021-09-20 20:17:13 +0300

    hr_contract_init: Migration to 14.0
    Split the module into two to reflect the split between HR and
    Payroll.
        - hr_contract_init
        - hr_contract_init_payroll

---
+++ hr_contract_init_payroll/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2021 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contract Payroll Structure Initial Settings",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "license": "AGPL-3",
+    "author": "TREVI Software, Michael Telahun Makonnen",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "group_payroll_manager",
+        "hr_contract",
+        "hr_contract_init",
+        "payroll",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_contract_init_view.xml",
+    ],
+    "installable": True,
+}
