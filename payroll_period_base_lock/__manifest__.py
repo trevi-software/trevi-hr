e196561865b1cd21997d154a545eac8e7ad00f1d
Michael Telahun <mtm@trevi.et>
2021-10-04 16:37:36 +0300

    [ADD] payroll_period_base_lock: implement base_lock for payroll periods

---
+++ payroll_period_base_lock/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2021 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Payroll Period Lock",
+    "summary": "Adds a base lock field to a payroll period.",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "author": "TREVI Software, Michael Telahun Makonnen",
+    "license": "AGPL-3",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": ["base_lock", "payroll_period"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
