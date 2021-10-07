07d757e64312ee4fa83fa0327c112a77df6cfbe1
Michael Telahun <mtm@trevi.et>
2021-10-07 16:28:40 +0300

    Massive module rename
    Most of my HR modules have the same name as in 7.0. This creates a
    module name conflic in the Odoo App Store because those modules
    are under my name and these modules are under TREVI's account. Therefore,
    the new modules do not appear in the app store. Most of the changes are
    straight renames with one notable exception. I have folded hr_security
    into hr_simplify and renamed it to trevi_hr_usability.

---
+++ hr_contract_status/models/hr_contract.py
@@ -0,0 +1,244 @@
+# Copyright (C) 2021 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import _, api, fields, models
+
+
+class HrContract(models.Model):
+
+    _name = "hr.contract"
+    _inherit = "hr.contract"
+
+    trial_ending = fields.Boolean()
+    state_ending = fields.Boolean()
+    date_end_effective = fields.Date(string="Effective End Date", readonly=True)
+    date_end_original = fields.Date(string="Original End Date", readonly=True)
+    state = fields.Selection(
+        selection_add=[
+            ("draft",),
+            ("trial", "Trial"),
+            ("open",),
+            ("close",),
+            ("cancel",),
+        ],
+        default="draft",
+        readonly=True,
+    )
+
+    department_id = fields.Many2one(
+        compute="_compute_department",
+        store=True,
+        readonly=True,
+    )
+
+    # At contract end this field will hold the job_id, and the
+    # job_id field will be set to null so that modules that
+    # reference job_id don't include deactivated employees.
+    # XXX ToDo: is it possible to change those references rather than using this hack?
+    end_job_id = fields.Many2one(
+        comodel_name="hr.job", string="Job Title", readonly=True
+    )
+
+    # The following are redefined again to make them editable only in certain states
+    employee_id = fields.Many2one(
+        readonly=True, states={"draft": [("readonly", False)]}
+    )
+    structure_type_id = fields.Many2one(
+        readonly=True, states={"draft": [("readonly", False)]}
+    )
+    job_id = fields.Many2one(
+        comodel_name="hr.job",
+        compute=False,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        required=False,
+        states={
+            "draft": [("required", True)],
+            "trial": [("required", True)],
+            "open": [("required", True)],
+        },
+        tracking=True,
+    )
+    date_start = fields.Date(
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+    )
+    wage = fields.Monetary(
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+            "trial": [("readonly", False)],
+            "trial_ending": [("readonly", False)],
+        },
+    )
+
+    @api.depends("job_id")
+    def _compute_department(self):
+        for contract in self:
+            contract.department_id = contract.job_id.department_id
+
+    # Override from inherited model. job_id and department_id in hr.employee should be
+    # calculated from the contract.
+    #
+    @api.depends("employee_id")
+    def _compute_employee_contract(self):
+        for contract in self.filtered("employee_id"):
+            contract.resource_calendar_id = contract.employee_id.resource_calendar_id
+            contract.company_id = contract.employee_id.company_id
+
+    @api.constrains("employee_id", "state", "kanban_state", "date_start", "date_end")
+    def _check_current_contract(self):
+
+        allow = self.env["ir.config_parameter"].get_param(
+            "hr_contract_state.concurrent_contracts", False
+        )
+        if allow:
+            return
+        return super(HrContract, self)._check_current_contract()
+
+    def _track_subtype(self, init_values):
+        self.ensure_one()
+        if "state" in init_values:
+            if self.state == "trial_ending":
+                return self.env.ref("hr_contract_state.mt_alert_trial_ending")
+            elif self.state == "contract_ending":
+                return self.env.ref("hr_contract_state.mt_alert_contract_ending")
+        return super(HrContract, self)._track_subtype(init_values)
+
+    @api.model
+    def update_state(self):
+
+        # New contract with trial period
+        self.search(
+            [
+                ("state", "=", "draft"),
+                ("kanban_state", "=", "done"),
+                ("date_start", "<=", fields.Date.to_string(date.today())),
+                ("trial_date_end", ">=", fields.Date.to_string(date.today())),
+            ]
+        ).write({"state": "trial"})
+
+        # Trial period is ending
+        contracts = self.search(
+            [
+                ("state", "=", "trial"),
+                (
+                    "trial_date_end",
+                    "<=",
+                    fields.Date.to_string(date.today() + relativedelta(days=7)),
+                ),
+            ]
+        )
+        for contract in contracts:
+            contract.kanban_state = "blocked"
+            contract.trial_ending = True
+            contract.activity_schedule(
+                "mail.mail_activity_data_todo",
+                contract.trial_date_end,
+                _("The trial period of %s is about to end.", contract.employee_id.name),
+                user_id=contract.hr_responsible_id.id or self.env.uid,
+            )
+
+        # Trial period has ended
+        contracts = self.search(
+            [
+                ("state", "=", "trial"),
+                (
+                    "trial_date_end",
+                    "<=",
+                    fields.Date.to_string(date.today() - relativedelta(days=1)),
+                ),
+            ]
+        )
+        for contract in contracts:
+            contract.state = "open"
+            contract.kanban_state = "normal"
+            contract.trial_ending = False
+
+        # Contract is expiring
+        contracts = self.search(
+            [
+                ("state", "=", "open"),
+                ("kanban_state", "!=", "blocked"),
+                "|",
+                "&",
+                (
+                    "date_end",
+                    "<=",
+                    fields.Date.to_string(date.today() + relativedelta(days=7)),
+                ),
+                (
+                    "date_end",
+                    ">=",
+                    fields.Date.to_string(date.today() + relativedelta(days=1)),
+                ),
+                "&",
+                (
+                    "visa_expire",
+                    "<=",
+                    fields.Date.to_string(date.today() + relativedelta(days=60)),
+                ),
+                (
+                    "visa_expire",
+                    ">=",
+                    fields.Date.to_string(date.today() + relativedelta(days=1)),
+                ),
+            ]
+        ).write({"state_ending": True})
+
+        # Contract has expired
+        self.search(
+            [
+                ("state", "=", "open"),
+                "|",
+                (
+                    "date_end",
+                    "<=",
+                    fields.Date.to_string(date.today() + relativedelta(days=1)),
+                ),
+                (
+                    "visa_expire",
+                    "<=",
+                    fields.Date.to_string(date.today() + relativedelta(days=1)),
+                ),
+            ]
+        ).write({"state_ending": False})
+
+        return super(HrContract, self).update_state()
+
+    def condition_trial_period(self):
+        self.ensure_one()
+        dToday = fields.Date.today()
+        if not self.trial_date_end or (
+            self.trial_date_end and self.trial_date_end < dToday
+        ):
+            return False
+        return True
+
+    def signal_confirm(self):
+        for rec in self:
+            if rec.kanban_state == "done":
+                rec.kanban_state = "normal"
+            if rec.condition_trial_period():
+                rec.state = "trial"
+            else:
+                rec.state = "open"
+        return
+
+    def signal_close(self):
+        for c in self:
+            vals = {"state": "close"}
+            if not c.date_end or c.date_end >= date.today():
+                vals.update({"date_end": date.today()})
+            c.write(vals)
+        return
+
+    def write(self, vals):
+        if vals.get("state") == "trial":
+            self._assign_open_contract()
+        return super(HrContract, self).write(vals)
---
+++ hr_contract_status/tests/test_hr_contract.py
@@ -0,0 +1,225 @@
+# Copyright (C) 2021 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
+from odoo.tests import common
+
+
+class TestContract(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestContract, cls).setUpClass()
+
+        cls.HrEmployee = cls.env["hr.employee"]
+        cls.HrContract = cls.env["hr.contract"]
+        cls.employee = cls.HrEmployee.create({"name": "John"})
+        cls.test_contract = dict(
+            name="Test", wage=1, employee_id=cls.employee.id, state="draft"
+        )
+
+    def create_contract(self, state, kanban_state, start, end=None, trial_end=None):
+        return self.env["hr.contract"].create(
+            {
+                "name": "Contract",
+                "employee_id": self.employee.id,
+                "state": state,
+                "kanban_state": kanban_state,
+                "wage": 1,
+                "date_start": start,
+                "trial_date_end": trial_end,
+                "date_end": end,
+            }
+        )
+
+    def apply_cron(self):
+        self.env.ref(
+            "hr_contract.ir_cron_data_contract_update_state"
+        ).method_direct_trigger()
+
+    def test_first_contract_open_notrial(self):
+        """The first contract when 'Running' goes to
+        'open' state, if trial end isn't set."""
+
+        start = datetime.now().date()
+        c = self.create_contract("draft", "normal", start)
+        c.signal_confirm()
+        self.assertEqual("open", c.state)
+        self.assertEqual("normal", c.kanban_state)
+
+    def test_first_contract_open_trial(self):
+        """The first contract when 'Running' goes to
+        trial period state, if trial end is set."""
+
+        start = datetime.now().date()
+        end = start + relativedelta(days=60)
+        c = self.create_contract("draft", "normal", start, trial_end=end)
+        c.signal_confirm()
+        self.assertEqual("trial", c.state)
+        self.assertEqual("normal", c.kanban_state)
+
+    def test_next_contract_open_running(self):
+        """Subsequent contracts go directly to 'Running' state"""
+
+        start = datetime.strptime("2015-11-01", "%Y-%m-%d").date()
+        end = datetime.strptime("2015-11-30", "%Y-%m-%d").date()
+        self.create_contract("close", "normal", start, end)
+
+        start = datetime.strptime("2015-12-01", "%Y-%m-%d").date()
+        c2 = self.create_contract("draft", "normal", start)
+        c2.signal_confirm()
+        self.assertEqual("open", c2.state)
+        self.assertEqual("normal", c2.kanban_state)
+
+    def test_start_trial(self):
+        """Cron update changes 'draft' contract into 'trial'"""
+
+        self.test_contract.update(
+            dict(
+                trial_date_end=datetime.now() + relativedelta(days=100),
+                kanban_state="done",
+            )
+        )
+        contract = self.HrContract.create(self.test_contract)
+        self.apply_cron()
+        self.assertEqual("trial", contract.state)
+        self.assertEqual("normal", contract.kanban_state)
+        self.assertEqual(False, contract.trial_ending)
+        self.assertEqual(False, self.employee.contract_warning)
+
+    def test_trial_ending(self):
+        self.test_contract.update(
+            dict(trial_date_end=datetime.now() + relativedelta(days=100))
+        )
+        contract = self.HrContract.create(self.test_contract)
+        contract.signal_confirm()
+        self.assertEqual("trial", contract.state)
+        self.assertEqual("normal", contract.kanban_state)
+        self.assertEqual(False, contract.trial_ending)
+        self.assertEqual(False, self.employee.contract_warning)
+
+        self.test_contract.update(
+            dict(trial_date_end=datetime.now() + relativedelta(days=5))
+        )
+        contract.write(self.test_contract)
+        self.apply_cron()
+        self.assertEqual(contract.state, "trial")
+        self.assertEqual(contract.trial_ending, True)
+        self.assertEqual(contract.kanban_state, "blocked")
+
+    def test_trial_ended(self):
+        self.test_contract.update(
+            {
+                "date_start": datetime.now() + relativedelta(days=-50),
+                "trial_date_end": datetime.now() + relativedelta(days=100),
+            }
+        )
+        contract = self.HrContract.create(self.test_contract)
+        contract.signal_confirm()
+        self.assertEqual("trial", contract.state)
+        self.assertEqual("normal", contract.kanban_state)
+        self.assertEqual(False, contract.trial_ending)
+        self.assertEqual(False, self.employee.contract_warning)
+
+        self.test_contract.update(
+            {
+                "trial_date_end": datetime.now() + relativedelta(days=-1),
+            }
+        )
+        contract.write(self.test_contract)
+        self.apply_cron()
+        self.assertEqual("open", contract.state)
+        self.assertEqual(False, contract.trial_ending)
+        self.assertEqual("normal", contract.kanban_state)
+
+    def test_contract_ending(self):
+        """When a contract nears completion the relevant boolean should be set"""
+
+        start = datetime.now().date()
+        end = start + relativedelta(days=100)
+        c = self.create_contract("draft", "normal", start, end)
+        c.signal_confirm()
+        self.assertEqual("open", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
+
+        c.date_end = start + relativedelta(days=5)
+        self.apply_cron()
+        self.assertEqual("open", c.state)
+        self.assertEqual("blocked", c.kanban_state)
+        self.assertTrue(c.state_ending)
+
+    def test_contract_ended(self):
+        """When a contract has ended the state should be 'close'"""
+
+        # Start
+        start = datetime.now().date() + relativedelta(days=-100)
+        today = datetime.now().date()
+        end = today + relativedelta(days=100)
+        c = self.create_contract("draft", "normal", start, end)
+        c.signal_confirm()
+        self.assertEqual("open", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
+
+        # Ending
+        c.date_end = today + relativedelta(days=5)
+        self.apply_cron()
+        self.assertEqual("open", c.state)
+        self.assertEqual("blocked", c.kanban_state)
+        self.assertTrue(c.state_ending)
+
+        # Ended
+        c.date_end = today + relativedelta(days=-1)
+        self.apply_cron()
+        self.assertEqual("close", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
+
+    def test_signal_close(self):
+        """Calling signal close immediately ends the contract"""
+
+        # Start
+        start = datetime.now().date() + relativedelta(days=-100)
+        today = datetime.now().date()
+        end = today + relativedelta(days=100)
+        c = self.create_contract("draft", "normal", start, end)
+        c.signal_confirm()
+        self.assertEqual("open", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
+
+        c.signal_close()
+
+        # Ended
+        self.assertEqual(today, c.date_end)
+        self.assertEqual("close", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
+
+    def test_signal_close_ended_contract(self):
+        """Calling signal_close() doesn't alter contract end date that is in the past"""
+
+        # Start
+        start = datetime.now().date() + relativedelta(days=-100)
+        today = datetime.now().date()
+        end = today + relativedelta(days=100)
+        c = self.create_contract("draft", "normal", start, end)
+        c.signal_confirm()
+        self.assertEqual("open", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
+
+        prev_end = today - relativedelta(days=1)
+        c.date_end = prev_end
+        c.signal_close()
+
+        # Ended
+        self.assertEqual(prev_end, c.date_end)
+        self.assertEqual("close", c.state)
+        self.assertEqual("normal", c.kanban_state)
+        self.assertFalse(c.state_ending)
