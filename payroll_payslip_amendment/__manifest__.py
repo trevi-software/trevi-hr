be22f5e2bb429d15f9da2a425ad318ab9361cf9c
Michael Telahun <mtm@trevi.et>
2022-01-25 16:15:58 +0300

    Migration of Payslip Amendments to 14.0
    It was split into 2 parts:
       - payslip amendments
       - linkage between payslip amendments and employee contracts to
         handle consecutive contracts within the payslip dates
    Without the second linkage module then payslip amendments would have to
    depend on hr_contract_status, which I did no want. Also, I ended up
    having to introduce a date into the payslip amendment because in earlier versions
    it was used in conjunction with payroll periods only. This would have
    introduced an unacceptable dependance on payroll_periods module. We will have
    to introduce another module to create a linkage with payroll periods.

---
+++ payroll_payslip_amendment/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Pay Slip Amendment",
+    "summary": "Add amendments to current and future pay slips",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "license": "AGPL-3",
+    "author": "TREVI Software, Michael Telahun Makonnen",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "hr",
+        "payroll",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_payslip_amendment_view.xml",
+    ],
+    "installable": True,
+}
---
+++ payroll_payslip_amendment_contract_status/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Payslip Amendment with Contract State",
+    "summary": "Link payslip amendments with the employee contract state.",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "license": "AGPL-3",
+    "author": "TREVI Software",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "hr_contract_status",
+        "payroll_payslip_amendment",
+    ],
+    "data": [],
+    "installable": True,
+}
