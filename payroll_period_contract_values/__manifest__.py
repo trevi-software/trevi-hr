4842b08e7e4ddd0b166dc11460a7d368a50b8f6a
Michael Telahun <mtm@trevi.et>
2022-02-02 09:06:51 +0300

    [ADD] payroll_period_contract_values: add pay period schedule to contract values

---
+++ payroll_period_contract_values/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Payroll Period Schedule in Contracts",
+    "summary": "Links payroll period schedules with employee contracts.",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "license": "AGPL-3",
+    "author": "TREVI Software, Michael Telahun Makonnen",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "hr_contract",
+        "hr_contract_values_payroll",
+        "payroll_period",
+    ],
+    "data": [
+        "views/hr_contract_init_view.xml",
+    ],
+    "installable": True,
+}
