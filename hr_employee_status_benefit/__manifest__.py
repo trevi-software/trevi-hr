64b4fc3711c35ce8bff9a87b6069fe6c487265ea
Michael Telahun <mtm@trevi.et>
2022-04-11 12:48:16 +0300

    [ADD] hr_employee_status_benefit: module to link employee status and benefits

---
+++ hr_employee_status_benefit/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Employee status benefit",
+    "summary": "Link between hr_employee_status and hr_benefit",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "images": ["static/src/img/main_screenshot.png"],
+    "license": "AGPL-3",
+    "author": "TREVI Software, Michael Telahun Makonnen",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": ["hr", "hr_benefit", "hr_employee_status"],
+    "data": [],
+    "installable": True,
+}
