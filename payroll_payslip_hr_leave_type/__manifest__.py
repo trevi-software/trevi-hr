84bc87bf420f7cb9cf14f3110651b7ba54ccba16
Michael Telahun <mtm@trevi.et>
2022-05-31 11:09:10 +0300

    [ADD] payroll_payslip_hr_leave_type: in salary rules use the code of the leave type instead of the name

---
+++ payroll_payslip_hr_leave_type/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Payslip Time-off codes",
+    "summary": "Use time-off codes (instead of names) in payslip rules",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "author": "TREVI Software",
+    "license": "AGPL-3",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "hr_leave_type_unique",
+        "payroll",
+    ],
+    "data": [],
+    "installable": True,
+}
