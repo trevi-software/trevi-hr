13854989f08e93090fb93564a4e34d34c8922cba
Michael Telahun <mtm@trevi.et>
2022-06-17 10:52:22 +0300

    payroll_policy_payslip - removed dependency that is now fixed in upstream

--- payroll_policy_payslip/__manifest__.py
+++ payroll_policy_payslip/__manifest__.py
@@ -16,7 +16,6 @@
         "hr_attendance_day",
         "hr_holidays_public",
         "payroll",
-        "payroll_payslip_hr_leave_type",
         "payroll_policy_absence",
         "payroll_policy_accrual",
         "payroll_policy_group",
