1c77e0b689e01c86563384b75bc38e846104d1d2
Michael Telahun <mtm@trevi.et>
2022-08-10 21:36:31 +0300

    [ADD] payroll_payslip_report: analyze payslips by department and salary rule category

---
+++ payroll_payslip_report/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Payslip Report",
+    "summary": "Comprehensive payslip report by department.",
+    "category": "Payroll",
+    "version": "14.0.1.0.0",
+    "author": "TREVI Software",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "images": ["static/src/img/main_screenshot.png"],
+    "license": "AGPL-3",
+    "depends": [
+        "payroll",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "report/hr_payslip_report.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
