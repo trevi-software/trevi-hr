78daabfac326307d95a6378c7618706fdeb7aabf
Michael Telahun <mtm@trevi.et>
2022-08-30 18:52:39 +0300

    [ADD] payroll_default_salary_rules: basic set of necessary salary rules and categories to go with them
    This module provides a very small set of essential salary rules and categories.
    It also allows the administrator to set the specific salary rule and
    salary rule category codes to be used in configuration settings. This
    should make developing payroll modules and reports easier since they
    can rely on the settings instead of specific codes.

---
+++ payroll_default_salary_rules/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Default Salary Rules and Categories",
+    "summary": "Default set of salary rules and categories.",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "author": "TREVI Software",
+    "license": "AGPL-3",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "payroll",
+    ],
+    "data": [
+        "data/default_salary_data.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "demo": [],
+    "installable": True,
+}
---
+++ payroll_default_salary_rules/models/res_config_settings.py
@@ -0,0 +1,77 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    categ_info = fields.Char(
+        string="Informational Categ.",
+        config_parameter="payroll_default_salary_rules.categ_info",
+    )
+
+    categ_ee_contribution = fields.Char(
+        string="Employee Contribution Categ.",
+        config_parameter="payroll_default_salary_rules.categ_ee_contribution",
+    )
+
+    categ_er_contribution = fields.Char(
+        string="Employer Contribution Categ.",
+        config_parameter="payroll_default_salary_rules.categ_er_contribution",
+    )
+
+    categ_basic = fields.Char(
+        string="Basic Categ.",
+        config_parameter="payroll_default_salary_rules.categ_basic",
+    )
+
+    categ_ot = fields.Char(string="OT Categ.", config_parameter=".categ_ot")
+
+    categ_allowance = fields.Char(
+        string="Allowance Categ.",
+        config_parameter="payroll_default_salary_rules.categ_allowance",
+    )
+
+    categ_taxexempt = fields.Char(
+        string="Tax Exempt Categ.",
+        config_parameter="payroll_default_salary_rules.categ_taxexempt",
+    )
+
+    categ_taxable = fields.Char(
+        string="Taxable Gross Categ.",
+        config_parameter="payroll_default_salary_rules.categ_taxable",
+    )
+
+    categ_gross = fields.Char(
+        string="Gross Categ.",
+        config_parameter="payroll_default_salary_rules.categ_gross",
+    )
+
+    categ_payroll_tax = fields.Char(
+        string="Payroll Tax Categ.",
+        config_parameter="payroll_default_salary_rules.categ_payroll_tax",
+    )
+
+    categ_income_tax = fields.Char(
+        string="Income Tax Categ.",
+        config_parameter="payroll_default_salary_rules.categ_income_tax",
+    )
+
+    categ_deduction = fields.Char(
+        string="Deduction Categ.",
+        config_parameter="payroll_default_salary_rules.categ_deduction",
+    )
+
+    categ_net = fields.Char(
+        string="Net Categ.", config_parameter="payroll_default_salary_rules.categ_net"
+    )
+
+    rule_gross = fields.Char(
+        string="Gross Rule", config_parameter="payroll_default_salary_rules.rule_gross"
+    )
+
+    rule_net = fields.Char(
+        string="Net Rule", config_parameter="payroll_default_salary_rules.rule_net"
+    )
