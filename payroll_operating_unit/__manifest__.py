89d93ff374d1af46c41fb37172272220db0b9504
Michael Telahun <mtm@trevi.et>
2022-09-07 11:58:11 +0300

    [ADD] payroll_operating_unit* modules
    These modules are here only to prevent the tests from failing because
    the PR created to the upstream OCA/operating-unit repo has not been
    merged yet. Once they get accepted these modules will be removed from
    this repo.

---
+++ payroll_operating_unit/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2022 Trevi Software (https://trevi.et)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "DO NOT USE - Payroll Operating Unit",
+    "summary": "WARNING-this module will be removed.",
+    "version": "14.0.1.0.0",
+    "category": "Payroll",
+    "images": ["static/src/img/main_screenshot.png"],
+    "author": "TREVI Software, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/trevi-software/trevi-hr",
+    "depends": [
+        "hr_contract_operating_unit",
+        "hr_operating_unit",
+        "operating_unit",
+        "payroll",
+        # XXX - This is to prevent Odoo's App store bot from picking this up
+        "hr_holidays_public",
+    ],
+    "data": [
+        "security/hr_payslip_security.xml",
+        "views/hr_payslip_view.xml",
+    ],
+    "installable": True,
+}
