c5109514235f00d8a59cb4c8489aa7c91c7ffa5f
Michael Telahun <mtm@trevi.et>
2023-04-27 10:52:13 +0300

    [ADD] hr_leave_type_unique_preinstall - module to add code field to default leave types

---
+++ hr_leave_type_unique_preinstall/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (C) 2023 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
--- hr_leave_type_unique_preinstall/__manifest__.py
+++ hr_leave_type_unique_preinstall/__manifest__.py
@@ -1,20 +1,21 @@
-# Copyright (C) 2021,2023 Trevi Software (https://trevi.et)
-# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# Copyright (C) 2023 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+# pylint: disable=pointless-statement
 {
-    "name": "Leave Type Unique",
-    "summary": "Ensure leave types are unique",
-    "version": "14.0.1.2.0",
-    "category": "Hidden",
+    "name": "Leave Type Unique - Preinstall",
+    "summary": "Technical module depended on by Leave Type Unique",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
     "license": "AGPL-3",
     "author": "TREVI Software, Michael Telahun Makonnen",
     "images": ["static/src/img/main_screenshot.png"],
     "website": "https://github.com/trevi-software/trevi-hr",
     "depends": [
         "hr_holidays",
-        "hr_leave_type_unique_preinstall",
     ],
-    "data": [],
+    "data": [
+        "data/hr_holidays_data.xml",
+    ],
     "installable": True,
 }
