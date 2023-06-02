55000049f41e8b7fdceba1d7db48c677195a6d25
Michael Telahun <mtm@trevi.et>
2023-06-02 11:33:35 +0300

    [UPD] Bump version minor; README.rst

--- hr_employee_seniority_months/__manifest__.py
+++ hr_employee_seniority_months/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Employee Seniority",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "license": "AGPL-3",
     "author": "TREVI Software",
     "category": "Human Resources",
