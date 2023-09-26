a3046528b26415c12700cb951e9987cea7d36fd2
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:53 +0300

    [MIG] payroll_payslip_report: Migration to 15.0

--- payroll_payslip_report/__manifest__.py
+++ payroll_payslip_report/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Payslip Report",
     "summary": "Comprehensive payslip report by department.",
     "category": "Payroll",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "author": "TREVI Software",
     "website": "https://github.com/trevi-software/trevi-hr",
     "images": ["static/src/img/main_screenshot.png"],
