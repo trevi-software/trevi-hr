0dd92c4475db7f73b9361550fb2274697eb0274b
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:53 +0300

    [MIG] payroll_policy_accrual: Migration to 15.0

--- payroll_policy_accrual/__manifest__.py
+++ payroll_policy_accrual/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Time Accrual Policy",
     "summary": "Automatically or manually accrue to time banks to be withdrawn later",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Payroll",
     "author": "TREVI Software, Michael Telahun Makonnen",
     "license": "AGPL-3",
