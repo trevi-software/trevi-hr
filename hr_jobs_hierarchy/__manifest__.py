1bbefd44cf0ec38afe99c1b3ead8ee2bb78cb910
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:52 +0300

    [MIG] hr_jobs_hierarchy: Migration to 15.0

--- hr_jobs_hierarchy/__manifest__.py
+++ hr_jobs_hierarchy/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Job Hierarchy",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "license": "AGPL-3",
     "author": "TREVI Software ",
