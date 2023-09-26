ff25ff0f046610bb4c9ee796ebf0602cf5b75b60
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:53 +0300

    [MIG] hr_employee_status_payroll: Migration to 15.0

--- hr_employee_status_payroll/__manifest__.py
+++ hr_employee_status_payroll/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Employment Status - Payroll Linkage",
     "summary": "Adds access records to employee separation records",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Payroll",
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
