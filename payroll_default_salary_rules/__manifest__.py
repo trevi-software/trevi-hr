5c1474f3d4172e13f7d30415d06faea4e4bb761c
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:53 +0300

    [MIG] payroll_default_salary_rules: Migration to 15.0

--- payroll_default_salary_rules/__manifest__.py
+++ payroll_default_salary_rules/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Default Salary Rules and Categories",
     "summary": "Default set of salary rules and categories.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Payroll",
     "images": ["static/src/img/main_screenshot.png"],
     "author": "TREVI Software",
