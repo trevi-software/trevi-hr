166b3632e4a1542370cfa676c1d59de5e1bcebf2
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:53 +0300

    [MIG] hr_data_import: Migration to 15.0

--- hr_data_import/__manifest__.py
+++ hr_data_import/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "HR Data Import",
     "summary": "Import HR data from another system using Excel",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
