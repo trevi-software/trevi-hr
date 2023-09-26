140b667ba898f45cbd2b2b26b9d7f5e4bf7c9237
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:52 +0300

    [MIG] payroll_payslip_hr_leave_type: Migration to 15.0

--- payroll_payslip_hr_leave_type/__manifest__.py
+++ payroll_payslip_hr_leave_type/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Payslip Time-off codes",
     "summary": "Use time-off codes (instead of names) in payslip rules",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Payroll",
     "images": ["static/src/img/main_screenshot.png"],
     "author": "TREVI Software",
