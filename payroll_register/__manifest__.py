9a8566d4b38e315ea7d0a7c2b5a1f8e106b58b86
Michael Telahun <mtm@trevi.et>
2023-09-26 11:35:53 +0300

    [MIG] payroll_register: Migration to 15.0

--- payroll_register/__manifest__.py
+++ payroll_register/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Payroll Register",
-    "version": "14.0.1.3.0",
+    "version": "15.0.1.0.0",
     "category": "Payroll",
     "license": "AGPL-3",
     "author": "TREVI Software, Michael Telahun Makonnen",
