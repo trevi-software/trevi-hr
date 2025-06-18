4c8912f232d78a0e98e834c9743116d041e44113
Michael Telahun Makonnen <mtm@trevi.et>
2025-06-18 11:39:41 +0300

    Refactor (use only lower-case variables)

--- hr_data_import/models/hr_employee.py
+++ hr_data_import/models/hr_employee.py
@@ -14,17 +14,17 @@ class HrEmployee(models.Model):
     )
     import_data_id = fields.Many2one("hr.data.import.employee", "Imported Record")
 
-    def get_months_service_to_date(self, dToday=None):
-        months = super().get_months_service_to_date(dToday)
+    def get_months_service_to_date(self, today=None):
+        months = super().get_months_service_to_date(today)
         if self.hire_date and self.hire_date < self.first_contract_date:
-            if dToday is None:
-                dToday = fields.Date.today()
+            if today is None:
+                today = fields.Date.today()
             delta = relativedelta(self.first_contract_date, self.hire_date)
             months += round(
                 float(
                     delta.years * 12
                     + delta.months
-                    + delta.days / float(self._get_days_in_month(dToday))
+                    + delta.days / float(self._get_days_in_month(today))
                 ),
                 2,
             )
