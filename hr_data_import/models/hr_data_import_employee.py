b7b6e351d36f6699a88d09ea1f852885f82b2093
Michael Telahun Makonnen <mtm@trevi.et>
2025-06-28 09:56:20 +0300

    Add annual leave records to hr.employee imported data and to hr.employee record

--- hr_data_import/models/hr_data_import_employee.py
+++ hr_data_import/models/hr_data_import_employee.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2021 Trevi Software (https://trevi.et)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from datetime import date, timedelta
 from odoo import _, fields, models
 from odoo.exceptions import UserError
 
@@ -57,6 +58,7 @@ class ImportEmployee(models.Model):
     emergency_contact = fields.Char("Emergency Contact", groups="hr.group_hr_user")
     emergency_phone = fields.Char("Emergency Phone", groups="hr.group_hr_user")
     hire_date = fields.Date(string="Date Hired", help="Initial date of employment.")
+    department_id = fields.Many2one("hr.department")
     job_id = fields.Many2one("hr.job", string="Job Position")
     date_start = fields.Date(
         "Start Date", required=True, help="Start date of the contract."
@@ -86,6 +88,11 @@ class ImportEmployee(models.Model):
     currency_id = fields.Many2one(
         string="Currency", related="company_id.currency_id", readonly=True
     )
+    DA_LEAVE="Product Price"
+    anlv_earned = fields.Float("Earned", digits=DA_LEAVE, default=0.00)
+    anlv_used = fields.Float("Used", digits=DA_LEAVE, default=0.00)
+    anlv_remain = fields.Float("Remaining", digits=DA_LEAVE, default=0.00)
+    anlv_date = fields.Date("As of")
 
     def action_import_employees(self):
         if self.filtered(lambda so: so.state != "new"):
@@ -95,94 +102,65 @@ class ImportEmployee(models.Model):
 
     def import_records(self):
 
-        Partner = self.env["res.partner"]
+        partner_obj = self.env["res.partner"]
 
         # Create the basic hr.employee record
         values_list = []
         for rec in self:
             # Create contact
-            _contact = Partner.create(
+            contact = partner_obj.create(
                 {
                     "name": rec.name,
                     "type": "private",
-                    "street": rec.street and rec.street or False,
-                    "mobile": rec.private_phone and rec.private_phone or False,
-                    "email": rec.private_email and rec.private_email or False,
-                    "vat": rec.taxid and rec.taxid or False,
+                    "street": (rec.street) and rec.street or False,
+                    "mobile": (rec.private_phone) and rec.private_phone or False,
+                    "email": (rec.private_email) and rec.private_email or False,
+                    "vat": (rec.taxid) and rec.taxid or False,
                 }
             )
             val = {
                 "name": rec.name,
                 "import_data_id": rec.id,
-                "address_home_id": _contact.id,
+                "address_home_id": contact.id,
+                "birthday": rec.birthday,
+                "place_of_birth": rec.place_of_birth,
+                "gender": rec.gender,
+                "marital": rec.marital,
+                "identification_id": rec.identification_id,
+                "private_email": rec.private_email,
+                "emergency_contact": rec.emergency_contact,
+                "emergency_phone": rec.emergency_phone,
+                "hire_date": rec.hire_date,
+
             }
-            if rec.birthday:
-                val.update({"birthday": rec.birthday})
-            if rec.place_of_birth:
-                val.update({"place_of_birth": rec.place_of_birth})
-            if rec.education_level:
-                if rec.education_level == "1":
-                    val.update({"study_field": "1"})
-                elif rec.education_level == "2":
-                    val.update({"study_field": "2"})
-                elif rec.education_level == "3":
-                    val.update({"study_field": "3"})
-                elif rec.education_level == "4":
-                    val.update({"study_field": "4"})
-                elif rec.education_level == "5":
-                    val.update({"study_field": "5"})
-                elif rec.education_level == "6":
-                    val.update({"study_field": "6"})
-                elif rec.education_level == "7":
-                    val.update({"study_field": "7"})
-                elif rec.education_level == "8":
-                    val.update({"study_field": "8"})
-                elif rec.education_level == "9":
-                    val.update({"study_field": "9"})
-                elif rec.education_level == "10":
-                    val.update({"study_field": "10"})
-                elif rec.education_level == "11":
-                    val.update({"study_field": "11"})
-                elif rec.education_level == "12":
-                    val.update({"study_field": "12"})
-                elif rec.education_level == "diploma":
-                    val.update({"certificate": "diploma"})
-                elif rec.education_level == "ba":
-                    val.update({"certificate": "bachelor"})
-            if rec.gender:
-                val.update({"gender": rec.gender})
-            if rec.marital:
-                val.update({"marital": rec.marital})
-            if rec.identification_id:
-                val.update({"identification_id": rec.identification_id})
-            if rec.private_email:
-                val.update({"private_email": rec.private_email})
-            if rec.emergency_contact:
-                val.update({"emergency_contact": rec.emergency_contact})
-            if rec.emergency_phone:
-                val.update({"emergency_phone": rec.emergency_phone})
-            if rec.hire_date:
-                val.update({"hire_date": rec.hire_date})
+            if rec.education_level in ["1","2","3","4","5","6","7","8","9","10","11","12"]:
+                val.update({"study_field": rec.education_level})
+            elif rec.education_level == "diploma":
+                val.update({"certificate": "diploma"})
+            elif rec.education_level == "ba":
+                val.update({"certificate": "bachelor"})
             values_list.append(val)
-        res = self.env["hr.employee"].create(values_list)
+
+        employees = self.env["hr.employee"].create(values_list)
 
         # Link the created employee to the import data record
         for rec in self:
-            rec.related_employee_id = res.filtered(
-                lambda e: e.import_data_id.id == rec.id
+            rec.related_employee_id = employees.filtered(
+                lambda e, rec=rec: e.import_data_id.id == rec.id
             )
 
         # Additional changes to system
-        contracts = self.create_contracts(res)
+        contracts = self.create_contracts(employees)
         contracts.signal_confirm()
+        # self.create_annual_leave_allocation(employees)
 
-        return res
+        return employees
 
     def create_contracts(self, employee_ids):
         contracts_list = []
         for ee in employee_ids:
-            data_id = self.filtered(lambda s: s.related_employee_id.id == ee.id)
-            contract_vals = {
+            data_id = self.filtered(lambda s, ee=ee: s.related_employee_id.id == ee.id)
+            records = {
                 "employee_id": ee.id,
                 "wage": data_id.wage,
                 "date_start": data_id.date_start,
@@ -193,14 +171,58 @@ class ImportEmployee(models.Model):
                 "kanban_state": "done",
             }
             if data_id.date_end:
-                contract_vals.update({"date_end": data_id.date_end})
+                records.update({"date_end": data_id.date_end})
             if data_id.trial_date_end:
-                contract_vals.update({"trial_date_end": data_id.trial_date_end})
+                records.update({"trial_date_end": data_id.trial_date_end})
             if data_id.resource_calendar_id:
-                contract_vals.update(
+                records.update(
                     {"resource_calendar_id": data_id.resource_calendar_id.id}
                 )
             if data_id.contract_type_id:
-                contract_vals.update({"contract_type_id": data_id.contract_type_id.id})
-            contracts_list.append(contract_vals)
+                records.update({"contract_type_id": data_id.contract_type_id.id})
+            contracts_list.append(records)
         return self.env["hr.contract"].create(contracts_list)
+    
+    def create_annual_leave_allocation(self, employees):
+        al_status_id = self.env["hr.leave.type"].search([("name", "=", "Annual Leave")])[0].id
+        records = {}
+        for ee in employees:
+            data = self.filtered(lambda s, ee=ee: s.related_employee_id.id == ee.id)
+            lv_days = self.calculate_leave_days_accrued(ee, data.hire_date)
+            # if data.anlv_earned != lv_days:
+            #     raise UserError("Calculated annual leave (data, calculated) {} - {} != {}"
+            #                     .format(
+            #                         ee.name, data.anlv_earned, lv_days)
+            #                     )
+            leave_allocation = {
+                "employee_id": ee.id,
+                "name": "Leave allocation for {} as of {}".format(data.name, date.today()),
+                "state": "draft",
+                "holiday_status_id": al_status_id,
+                "number_of_days": lv_days,
+            }
+            records.append(leave_allocation)
+        
+        return self.env["hr.leave.allocation"].create(records)
+    
+    def calculate_leave_days_accrued(self, employee, hire_date):
+
+        today = date.today()
+        accrued_todate = 0
+        extra_accrued = 0
+        one_day = 0
+        tmp_date = hire_date + timedelta(weeks=52, days=1)
+        while today >= tmp_date:
+            accrued_todate += 20 + extra_accrued + one_day
+            tmp_date += timedelta(weeks=52)
+            one_day = 1
+            extra_accrued += one_day
+        if tmp_date > today:
+            tmp_date -= timedelta(weeks=52)
+            extra_accrued -= one_day
+        monthly_accrual = (20 + extra_accrued) / 12.0
+        delta = today - tmp_date
+        if delta.days > 0:
+            accrued_todate += (delta.days / 30) * monthly_accrual
+        
+        return accrued_todate
--- hr_data_import/models/hr_employee.py
+++ hr_data_import/models/hr_employee.py
@@ -13,6 +13,11 @@ class HrEmployee(models.Model):
         help="Initial date of employment if different than date on first contract."
     )
     import_data_id = fields.Many2one("hr.data.import.employee", "Imported Record")
+    DA_LEAVE="Product Price"
+    anlv_earned = fields.Float("Earned", digits=DA_LEAVE, default=0.00)
+    anlv_used = fields.Float("Used", digits=DA_LEAVE, default=0.00)
+    anlv_remain = fields.Float("Remaining", digits=DA_LEAVE, default=0.00)
+    anlv_date = fields.Date("As of")
 
     def get_months_service_to_date(self, today=None):
         months = super().get_months_service_to_date(today)
