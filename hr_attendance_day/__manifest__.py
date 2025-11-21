19a06227d195a83733417be4d23051a41443fcf4
Michael Telahun Makonnen <mtm@trevi.et>
2025-11-21 10:39:39 +0300

    [MIG] hr_attendance_day: Migration to 18.0

--- hr_attendance_day/__manifest__.py
+++ hr_attendance_day/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Attendance Day",
     "summary": "Attach a localized date to an attendace record",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Human Resources",
     "author": "TREVI Software",
     "license": "AGPL-3",
