b5b919745c780866569197173e8d89e9d4dee8b4
Michael Telahun Makonnen <mtm@trevi.et>
2025-11-21 10:39:37 +0300

    [MIG] hr_contract_values: Migration to 18.0

--- hr_contract_values/__manifest__.py
+++ hr_contract_values/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Contracts - Initial Settings",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Human Resources",
     "license": "AGPL-3",
     "author": "TREVI Software, Michael Telahun Makonnen",
