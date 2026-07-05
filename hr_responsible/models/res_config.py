f1d99f86de0409aec754037f1c4fc1da1854a249
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-05 11:00:16 +0300

    [ADD] hr_responsible: set an hr.employee record designated as HR Responsible for all employees

---
+++ hr_responsible/models/res_config.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2021 Trevi Software (https://trevi.et)
+# Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import fields, models
+
+
+class ResConfig(models.TransientModel):
+
+    _inherit = "res.config.settings"
+
+    hr_responsible = fields.Boolean(
+        string="Default HR Responsible",
+        config_parameter="hr_responsible.hr_responsible",
+        default=False,
+        help="Set the default HR responsible for employees",
+    )
