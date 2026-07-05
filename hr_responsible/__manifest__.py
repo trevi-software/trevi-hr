f944328dc74a38580c2f271db84cedc905b4a4a8
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-05 11:00:16 +0300

    [ADD] hr_responsible: set an hr.employee record designated as HR Responsible for all employees

---
+++ hr_responsible/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': "hr_responsible",
+
+    'summary': """
+        Set a default HR responsible for employees.""",
+
+    'description': """
+        This module allows you to set a default HR responsible for employees.
+    """,
+
+    'author': "TREVI Software",
+    'website': "https://github.com/trevi-software/trevi-hr",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '16.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['hr'],
+
+    # always loaded
+    'data': [
+        'views/res_config_view.xml',
+    ],
+}
