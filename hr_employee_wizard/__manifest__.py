e43e91a1d420a6188ab8ecca20b94bb0d1624bb8
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-16 16:03:12 +0300

    Migrate module versions

--- hr_employee_wizard/__manifest__.py
+++ hr_employee_wizard/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "New Employee Wizard",
     "summary": "Streamline the creation of a new employee record",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Human Resources",
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
