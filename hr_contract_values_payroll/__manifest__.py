a0b6402f498e6db82503e58e0f81bdcd5822dece
Michael Telahun Makonnen <mtm@trevi.et>
2026-08-07 20:02:19 +0300

    [18.0][MIG] hr_contract_values_payroll: Migrate to 18.0

--- hr_contract_values_payroll/__manifest__.py
+++ hr_contract_values_payroll/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Contract Payroll Structure Initial Settings",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Payroll",
     "license": "AGPL-3",
     "author": "TREVI Software, Michael Telahun Makonnen",
