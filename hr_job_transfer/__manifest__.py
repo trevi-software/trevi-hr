447a2d66e4c2f7f859aaf029fb31df0952ec26ee
Michael Telahun Makonnen <mtm@trevi.et>
2026-08-20 10:51:18 +0300

    [MIG] hr_job_transfer: Migrate to 18.0

--- hr_job_transfer/__manifest__.py
+++ hr_job_transfer/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Departmental Transfer",
     "category": "HR",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "images": ["static/src/img/main_screenshot.png"],
     "author": "TREVI Software, Michael Telahun Makonnen",
     "license": "AGPL-3",
